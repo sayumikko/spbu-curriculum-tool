9ff3069cc572e163d65d66e39e00c33b6244efce
sayumikko <kuksant2708@mail.ru>
2022-11-08 19:10:51 +0300

    Initial commit

---
+++ Main/Program.fs
@@ -0,0 +1,30 @@
+﻿open Checks
+open System.IO
+open CurriculumParser
+
+let plansFolder = "../WorkingPlans"
+
+let planNameToCode fileName = FileInfo(fileName).Name.Substring(3, 9)
+
+let planCodeToFileName planCode =
+    Directory.EnumerateFiles(System.AppDomain.CurrentDomain.BaseDirectory + "/../../../" + plansFolder)
+    |> Seq.find (fun f -> planNameToCode f = planCode)
+
+let print_help () =
+    printfn "Это инструмент для работы с учебными планами СПбГУ"
+    printfn "Введите номер учебного плана и параметры предупреждений"
+    printfn "Имеющиеся учебные планы:"
+
+    Directory.EnumerateFiles(plansFolder)
+    |> Seq.map (fun p -> FileInfo(p).Name.Substring(3, 9))
+    |> Seq.iter (printf "%s ")
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length = 0 then
+        print_help ()
+    else
+        let curriculum = DocxCurriculum(planCodeToFileName argv[0]) //todo: add wrong name handler
+        Checks.checks curriculum
+
+    0
---
+++ Warnings/Warnings.fs
@@ -0,0 +1,13 @@
+﻿namespace Checks
+
+open CurriculumParser
+
+module Checks =
+
+    let competences (curriculum: DocxCurriculum) =
+        curriculum.Disciplines
+        |> Seq.iter (fun d ->
+            d.Implementations
+            |> Seq.iter (fun d -> d.Competences |> Seq.iter (fun d -> printfn "%s" d.Code)))
+
+    let checks (curriculum: DocxCurriculum) = competences curriculum
