e2078bb37448784fd5c2b84bc4605a15fb63f273
sayumikko <kuksant2708@mail.ru>
2022-11-09 15:38:52 +0300

    Helper template added

--- Warnings/Warnings.fs
+++ Warnings/Warnings.fs
@@ -4,10 +4,14 @@ open CurriculumParser
 
 module Checks =
 
+    let hours (curriculum: DocxCurriculum) =
+        printfn "Здесь будет логика проверки зачетных часов"
+
     let competences (curriculum: DocxCurriculum) =
-        curriculum.Disciplines
-        |> Seq.iter (fun d ->
-            d.Implementations
-            |> Seq.iter (fun d -> d.Competences |> Seq.iter (fun d -> printfn "%s" d.Code)))
+        // curriculum.Disciplines
+        // |> Seq.iter (fun d ->
+        //     d.Implementations
+        //     |> Seq.iter (fun d -> d.Competences |> Seq.iter (fun d -> printfn "%s" d.Code)))
+        printfn "Здесь будет логика проверки компетенций"
 
     let checks (curriculum: DocxCurriculum) = competences curriculum
