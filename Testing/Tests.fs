75e0780d789a9a60a591f90cf25ce91cdb00e012
sayumikko <kuksant2708@mail.ru>
2022-11-28 23:36:43 +0300

    Fixed paths

--- Testing/Tests.fs
+++ Testing/Tests.fs
@@ -14,16 +14,11 @@ type MyTests() =
 
 [<Test>]
 let competence_check () =
-    let path =
-        "/home/sayumiko/Курсовая/spbu-curriculum-tool/Testing/test_curricula.docx"
+    let curriculum = DocxCurriculum(System.AppDomain.CurrentDomain.BaseDirectory + "/../../../test_curricula.docx")
 
-    let curriculum = DocxCurriculum(path)
     Warnings.competences curriculum |> should equalSeq (seq {"ТК-1"}) 
 
 [<Test>]
 let hours_check () =
-    let path =
-        "/home/sayumiko/Курсовая/spbu-curriculum-tool/Testing/test_curricula.docx"
-
-    let curriculum = DocxCurriculum(path)
+    let curriculum = DocxCurriculum(System.AppDomain.CurrentDomain.BaseDirectory + "/../../../test_curricula.docx")
     Warnings.hours curriculum |> should equalSeq (seq {(41,1); (33,2); (33,3); (33,4)})
